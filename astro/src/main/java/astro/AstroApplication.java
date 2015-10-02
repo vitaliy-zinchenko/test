23fb637479961e0ff3a4c813d54775b9490325d0
Vitaliy Zinchenko <Vitaliy_Zinchenko@epam.com>
2015-10-02 19:17:20 +0300

    astro

---
+++ astro/src/main/java/astro/AstroApplication.java
@@ -0,0 +1,12 @@
+package astro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstroApplication.class, args);
+    }
+}
---
+++ astro/src/main/java/astro/ServletInitializer.java
@@ -0,0 +1,13 @@
+package astro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AstroApplication.class);
+	}
+
+}
